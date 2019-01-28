978ffbd36f18937be45cb1075e0a5a7beb6dceb0
aprv04 <46750701+aprv04@users.noreply.github.com>
2019-01-28 16:36:32 +0530

    Day3

--- q1.c
+++ q1.c
@@ -10,43 +10,48 @@
 
 // Define here the global static variables
 //
-int flag;
+int My_global;
 
 // Interrupt handler example for INT0
 //
-ISR(TIMER2_COMP_vect) {
-   flag^=1;
+SIGNAL(SIG_INTERRUPT0) {
+
 }
 
 // It is recommended to use this coding style to
 // follow better the mixed C-assembly code in the
 // Program Memory window
 //
-//void my_function(void) {  // Put the open brace '{' here
+void my_function(void) {  // Put the open brace '{' here
 
-  // asm("nop");          // Inline assembly example
-//}
+   asm("nop");          // Inline assembly example
+}
 
 // ***********************************************************
 // Main program
 //
-int main(void) {
-	      DDRB=0x02;
-	     TCNT2=0;
-	     OCR2=156;
-	     TCCR2|=(1<<CS22)|(1<<WGM21);
-	     TIMSK|=(1<<OCIE2);
-	     sei();
-
-   while(1) {             // Infinite loop; define here the
-   	if(flag)
-   		PORTB=0x55 ;
-   	else
-   		PORTB=0xAA;
-   		
-//      my_function();      // system behaviour
-   }
-
+void uart_init(void)
+{
+	UBRRL=0x33;
+	UCSRB=(1<<TXEN) | (1<<RXEN);
+	UCSRC=(1<<UCSZ0) | (1<<UCSZ1) | (1<<URSEL);
+	
 }
-
+void uart_send(unsigned char c)
+{
+	while(!(UCSRA & (1<<UDRE)));
+	UDR=c;
+}
+int main(void) {
+     uart_init() ;
+     unsigned char c;
+   while(1) {             // Infinite loop; define here the
+     // my_function();      // system behaviour
+     while(!(UCSRA & (1<<RXC)));
+     c=UDR;
+     c+=1;
+     uart_send(c);
+   }
+    return 0;
+}
 
--- q2.c
+++ q2.c
@@ -15,49 +15,33 @@ volatile int flag;
 // Interrupt handler example for INT0
 //
 ISR(INT0_vect) {
-   // TCNT2=0;
-    OCR2=125;
-}
-ISR(INT1_vect) {
-   // TCNT2=0;
-    OCR2=250 ;
-}
-ISR(TIMER2_COMP_vect) {
-	flag^=1;
+        flag^=1;
 }
 
 // It is recommended to use this coding style to
 // follow better the mixed C-assembly code in the
 // Program Memory window
 //
-//void my_function(void) {  // Put the open brace '{' here
+void my_function(void) {  // Put the open brace '{' here
 
-  // asm("nop");          // Inline assembly example
-//}
+   asm("nop");          // Inline assembly example
+}
 
 // ***********************************************************
 // Main program
 //
 int main(void) {
-	TCCR2=(1<<CS21)|(1<<WGM21);
-	TCNT2=0;
-	OCR2=250;
-	TIMSK=(1<<OCIE2);
-	GICR=(1<<INT0)|(1<<INT1);
-	sei();
-	DDRB=0xFF;
-	DDRD=0x00;
-	PORTD=0xFF;
-	
-
+		DDRD=0x10;
+		PORTD=0x04;
+      GICR=(1<<INT0);
+      sei();
    while(1) {             // Infinite loop; define here the
- 				if(flag)
- 					PORTB=0x55;
- 				else
- 					PORTB=0xAA;
- //   my_function();      // system behaviour
+     // my_function();      // system behaviour
+        if(flag)
+        		PORTD=0x04;
+        else
+        		PORTD=0x14;
    }
 
 }
-
 
